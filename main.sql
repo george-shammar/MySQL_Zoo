2fc9cfea9a69c752daa5a67657966be60cf3501c
George <george.gbenle@outlook.com>
2021-01-29 22:14:38 +0000

    Solve task 2 in Select basic section

--- main.sql
+++ main.sql
@@ -1,4 +1,9 @@
 -- select basic
 
-SELECT population FROM world
-  WHERE name = 'Germany'
+SELECT population 
+FROM world
+WHERE name = 'Germany'
+
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
