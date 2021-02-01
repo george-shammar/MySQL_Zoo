b44f8720158e49f10dd8baa939a53f50f0b84b5e
George <george.gbenle@outlook.com>
2021-02-01 20:46:10 +0000

    Solve task 2

--- main.sql
+++ main.sql
@@ -140,3 +140,10 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia');
+
+SELECT name
+FROM world
+WHERE continent = 'Europe' AND GDP/population > 
+(SELECT GDP/population 
+FROM world
+WHERE name = 'United Kingdom');
