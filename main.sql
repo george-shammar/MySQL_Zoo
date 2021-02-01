4a4db52679995a6b65ae797807a9108d032ceb87
George <george.gbenle@outlook.com>
2021-02-01 12:41:51 +0000

    Solve task 13

--- main.sql
+++ main.sql
@@ -60,3 +60,8 @@ WHERE LEN(name) = LEN(capital);
 SELECT name, capital
 FROM world
 WHERE LEFT(name,1) = LEFT(capital, 1) AND name<>capital;
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
+  AND name NOT LIKE '% %';
