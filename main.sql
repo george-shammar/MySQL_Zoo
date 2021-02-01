abab91b215a3df8fe49484b8808465c7b3c3c8e4
George <george.gbenle@outlook.com>
2021-02-01 19:32:31 +0000

    Solve task 9

--- main.sql
+++ main.sql
@@ -102,3 +102,7 @@ WHERE winner LIKE ('John%');
 SELECT yr, subject, winner
 FROM nobel
 WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980 AND subject != 'Chemistry' AND subject != 'Medicine';
