6311030fe89fa82f8a2c88e364e06c221f5c794d
George <george.gbenle@outlook.com>
2021-02-01 20:34:17 +0000

    Solve task 14

--- main.sql
+++ main.sql
@@ -123,3 +123,13 @@ SELECT winner, yr, subject
 FROM nobel
 WHERE winner LIKE 'Sir%'
 ORDER BY yr DESC, winner;
+
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY 
+CASE
+WHEN subject IN ('Physics', 'Chemistry') THEN 1
+ELSE 0
+END,
+subject, winner;
