dd4e2fd0317ef1bb56e9088d4d67c2b8d67a9753
George <george.gbenle@outlook.com>
2021-02-01 12:52:29 +0000

    Solve task 2

--- main.sql
+++ main.sql
@@ -72,6 +72,11 @@ SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
 
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
 SELECT yr, subject
 FROM nobel
 WHERE winner = 'Albert Einstein';
