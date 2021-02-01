2c79465e701d566ef27c207efb6bdfccbb55e755
George <george.gbenle@outlook.com>
2021-02-01 13:00:44 +0000

    SOlve task 6

--- main.sql
+++ main.sql
@@ -88,3 +88,9 @@ WHERE subject = 'peace' AND yr >= 2000;
 SELECT *
 FROM nobel
 WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989; 
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                    'Barack Obama');
