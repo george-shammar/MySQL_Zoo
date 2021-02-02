bfc787bc09df3cd35a7229f5ced1ef9c254a52e6
George <george.gbenle@outlook.com>
2021-02-02 20:14:34 +0000

    Solve task 11

--- main.sql
+++ main.sql
@@ -312,3 +312,10 @@ WHERE ord != 1 AND actorid =
 (SELECT id
 FROM actor
 WHERE name = 'Harrison Ford');
+
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 1
