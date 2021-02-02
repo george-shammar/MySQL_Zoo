243efbb71b58ceaf93931a993826deb56bd36ae1
George <george.gbenle@outlook.com>
2021-02-02 21:17:34 +0000

    COrrect code

--- main.sql
+++ main.sql
@@ -62,9 +62,9 @@ FROM world
 WHERE LEFT(name,1) = LEFT(capital, 1) AND name<>capital;
 
 SELECT name
-   FROM world
+FROM world
 WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
-  AND name NOT LIKE '% %';
+AND name NOT LIKE '% %';
 
 --   nobel table
 
