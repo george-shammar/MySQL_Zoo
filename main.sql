25416fe388019e00379b4efdca251ebd22ff896b
George <george.gbenle@outlook.com>
2021-02-02 17:45:44 +0000

    Solve task 8

--- main.sql
+++ main.sql
@@ -241,3 +241,7 @@ WHERE coach = 'Fernando Santos';
 SELECT player
 FROM goal JOIN game ON (goal.matchid = game.id)
 WHERE stadium = 'National Stadium, Warsaw';
+
+SELECT DISTINCT(player)
+  FROM goal JOIN game ON (goal.matchid = game.id) 
+    WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid != 'GER';
