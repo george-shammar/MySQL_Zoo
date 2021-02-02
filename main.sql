1c4f409cb4e2a5ba93db1e637ab9eff07c4b580f
George <george.gbenle@outlook.com>
2021-02-02 12:48:59 +0000

    Solve task 5

--- main.sql
+++ main.sql
@@ -194,3 +194,7 @@ WHERE continent = 'Africa';
 SELECT COUNT(name)
 FROM world
 WHERE area >=1000000;
+
+SELECT SUM(population)
+FROM world
+WHERE name = 'Estonia' OR name = 'Latvia' OR name = 'Lithuania';
