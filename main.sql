27caf30c8faa892b3e79893005fdae87ba0713bd
George <george.gbenle@outlook.com>
2021-02-02 18:49:24 +0000

    Solve task 3

--- main.sql
+++ main.sql
@@ -271,6 +271,11 @@ SELECT id, title
  FROM movie
  WHERE yr=1962;
 
- SELECT yr
+SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane';
+
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr;
