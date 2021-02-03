0a73c86e455f2af786ff8239953d14b9b5f41172
George <george.gbenle@outlook.com>
2021-02-03 10:07:39 +0000

    Solve task 7

--- main.sql
+++ main.sql
@@ -358,3 +358,7 @@ FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
 
 SELECT COUNT(teacher.name), COUNT(mobile)
 FROM teacher;
+
+SELECT dept.name, COUNT(teacher.name) 
+FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id)
+GROUP BY dept.name;
