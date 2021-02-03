ce8e37baaec752ce6902c42b8adbc258248e0d16
George <george.gbenle@outlook.com>
2021-02-03 11:50:17 +0000

    Correct indentation

--- main.sql
+++ main.sql
@@ -400,12 +400,12 @@ WHERE a.stop = 53
 AND b.stop = 149
 
 SELECT a.company, a.num, stopa.name, stopb.name
-  FROM route a JOIN route b ON
-    (a.company=b.company AND a.num=b.num)
-    JOIN stops stopa ON (a.stop=stopa.id)
-    JOIN stops stopb ON (b.stop=stopb.id)
-  WHERE stopa.name='Craiglockhart'
-    AND stopb.name = 'London Road'
+FROM route a JOIN route b ON
+(a.company=b.company AND a.num=b.num)
+JOIN stops stopa ON (a.stop=stopa.id)
+JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+AND stopb.name = 'London Road'
 
 SELECT DISTINCT a.company, a.num
 FROM route a JOIN route b ON
