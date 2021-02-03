48d75e5af437d3207aec098a31b86d622aab7258
George <george.gbenle@outlook.com>
2021-02-03 11:59:15 +0000

    format code

--- main.sql
+++ main.sql
@@ -166,9 +166,8 @@ WHERE name = 'Poland');
 
 SELECT name, 
 CONCAT(ROUND(population/(SELECT population
-                                FROM world
-                                WHERE name = 'Germany')*100, 0), '%')AS percentage
-           
+ROM world
+WHERE name = 'Germany')*100, 0), '%')AS percentage
 FROM world
 WHERE continent = 'Europe';
 
