13c62051b37871b81626eb803600ee70a070f3d3
George <george.gbenle@outlook.com>
2021-02-03 12:04:07 +0000

    Correct indentation

--- main.sql
+++ main.sql
@@ -69,10 +69,10 @@ SELECT yr, subject, winner
 FROM nobel
 WHERE yr = 1950;
 
- SELECT winner
- FROM nobel
- WHERE yr = 1962
- AND subject = 'Literature';
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature';
 
 SELECT yr, subject
 FROM nobel
