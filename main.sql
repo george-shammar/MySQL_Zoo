07d8c5628aaa6499798f09780ef4153b6a31bf1f
George <george.gbenle@outlook.com>
2021-02-03 09:14:15 +0000

    Solve task 3

--- main.sql
+++ main.sql
@@ -341,3 +341,7 @@ WHERE dept IS NULL;
 SELECT teacher.name, dept.name
  FROM teacher INNER JOIN dept
            ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
